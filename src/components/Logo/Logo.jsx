60e9646e2bce1be93f1c0d2a8b831fd64dbedc14
kalin0l <aleksiev2494@gmail.com>
2022-01-19 21:49:35 +0200

    renaiming folder

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,13 +1,12 @@
 import React from "react";
 
-
 const Logo = (type) => {
-    console.log(type)
-    return (
-        <>
-        <img src={type.type.src} alt="img"/>
-        </>
-    )
-}
+  console.log(type);
+  return (
+    <>
+      <img src={type.type.src} alt="img" />
+    </>
+  );
+};
 
 export default Logo;
