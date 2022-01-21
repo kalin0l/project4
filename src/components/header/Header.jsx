f65d681e7d4b4a8efde2aa5ca8ede31bf9872bf6
kalin0l <aleksiev2494@gmail.com>
2022-01-21 18:31:51 +0200

    changes

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,24 +1,22 @@
 import React from "react";
-import Item from "@mui/material/ListItem";
 import Grid from "@mui/material/Grid";
 import Logo from "../logo/Logo";
-import defaultLogo from "../../../public/images/logo.svg";
 import Button from "@mui/material/Button";
 import SearchIcon from "@mui/icons-material/Search";
 import TextField from "@mui/material/TextField";
 import InputAdornment from "@mui/material/InputAdornment";
-import { Link } from "@mui/material";
+import { Link, ListItem } from "@mui/material";
+import styles from "./Header.module.css";
+import classNames from "classnames";
+import Container from "@mui/material/Container/Container";
 
-export default function Header(){
+export default function Header() {
   return (
     <Grid container>
-      <Grid item xs={4} md={4}>
-        <Item>
-          <Logo type={defaultLogo} />
-        </Item>
-      </Grid>
-      <Grid item xs={4} md={4}>
-        <Item>
+        <Grid item xs={4} md={4}>
+          <Logo type="default" />
+        </Grid>
+        <Grid item xs={4} md={4}>
           <TextField
             fullWidth
             id="input-with-icon-textfield"
@@ -32,15 +30,20 @@ export default function Header(){
             }}
             variant="standard"
           />
-        </Item>
-      </Grid>
+        </Grid>
       <Grid item xs={4} md={4}>
-        <Item>
-         <Link underline="none" color="primary" >Home</Link>
-         <Link underline="none" color="primary" sx={{ml:2,mr:2}} >Activity</Link>
-          <Button variant="contained">Explore</Button>
-        </Item>
+        <Link underline="none" color="primary">
+          Home
+        </Link>
+        <Link
+          underline="none"
+          className={classNames(styles.$text_primary)}
+          sx={{ ml: 2, mr: 2 }}
+        >
+          Activity
+        </Link>
+        <Button variant="contained">Explore</Button>
       </Grid>
     </Grid>
   );
-};
+}
