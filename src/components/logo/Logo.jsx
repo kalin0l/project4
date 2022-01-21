64f3f42642408f54b4f4d8a5023249993b344a10
kalin0l <aleksiev2494@gmail.com>
2022-01-21 16:01:14 +0200

    change

--- src/components/logo/Logo.jsx
+++ src/components/logo/Logo.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import mutedLogo from '../../../public/images/logo-muted.svg'
+import defaultLogo from '../../../public/images/logo.svg';
 
 
 const Logo = ({type}) => {
-    console.log(type)
+    console.log(type,defaultLogo,mutedLogo)
     return (
         <>
-        {!type ? <img src={mutedLogo.src} alt="muted"/> :  <img src={type.src} alt="unmuted"/>}
+        {!type ? <img src={mutedLogo.src} alt="muted"/> :  <img src={defaultLogo.src} alt="unmuted"/>}
         </>
     )
 }
