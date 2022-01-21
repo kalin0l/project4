e057bdd5bf11890d298a2337e2076c9c1eea386d
kalin0l <aleksiev2494@gmail.com>
2022-01-21 16:40:47 +0200

    change in the src

--- src/components/example/Example.jsx
+++ src/components/example/Example.jsx
@@ -3,7 +3,6 @@ import Container from "@mui/material/Container";
 import Link from "../link/Link";
 import styles from "./Example.module.scss";
 import classNames from "classnames";
-import defaultLogo from '../../../public/images/logo.svg';
 import Logo from "../logo/Logo";
 
 
--- src/components/logo/Logo.jsx
+++ src/components/logo/Logo.jsx
@@ -4,10 +4,9 @@ import defaultLogo from '../../../public/images/logo.svg';
 
 
 const Logo = ({type}) => {
-    console.log(mutedLogo)
     return (
         <>
-        {type === 'default' || !type ? <img src="/_next/static/image/public/images/logo.6b59795bbe7c48890d55a096f225136b.svg" alt="unmuted"/> : <img src="/_next/static/image/public/images/logo-muted.2f8352c43fd037ed40b582fbeea23296.svg" alt="muted"/> }
+        {type === 'default' || !type ? <img src={defaultLogo.src} alt="unmuted"/> : <img src={mutedLogo.src} alt="muted"/> }
         </>
     )
 }
