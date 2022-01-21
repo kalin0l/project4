bfbd7078c75fab1f2d261975cb3f4296d818a5b3
kalin0l <aleksiev2494@gmail.com>
2022-01-21 16:30:45 +0200

    change of the prop

--- src/components/logo/Logo.jsx
+++ src/components/logo/Logo.jsx
@@ -7,7 +7,7 @@ const Logo = ({type}) => {
     console.log(mutedLogo)
     return (
         <>
-        {type ? <img src="/_next/static/image/public/images/logo.6b59795bbe7c48890d55a096f225136b.svg" alt="unmuted"/> : <img src="/_next/static/image/public/images/logo-muted.2f8352c43fd037ed40b582fbeea23296.svg" alt="muted"/> }
+        {type === 'unmuted' ? <img src="/_next/static/image/public/images/logo.6b59795bbe7c48890d55a096f225136b.svg" alt="unmuted"/> : <img src="/_next/static/image/public/images/logo-muted.2f8352c43fd037ed40b582fbeea23296.svg" alt="muted"/> }
         </>
     )
 }
