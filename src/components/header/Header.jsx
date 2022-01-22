627ff6b537599ecd1428b773c657ef19abdc81d7
kalin0l <aleksiev2494@gmail.com>
2022-01-22 14:35:56 +0200

    adding items

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -6,43 +6,57 @@ import Button from "@mui/material/Button";
 import SearchIcon from "@mui/icons-material/Search";
 import TextField from "@mui/material/TextField";
 import InputAdornment from "@mui/material/InputAdornment";
-import { Link, ListItem } from "@mui/material";
+import { Link } from "@mui/material";
+import Paper from "@mui/material/Paper";
 import styles from "./Header.module.css";
+import { styled } from '@mui/material/styles';
 
 export default function Header() {
+  const Item = styled(Paper)(({ theme }) => ({
+    ...theme.typography.body2,
+    padding: theme.spacing(1),
+    textAlign: "center",
+    color: theme.palette.text.secondary,
+  }));
   return (
-    <Container fixed>
+    <Container maxWidth="xl">
       <Grid container>
         <Grid item xs={4} md={4}>
-          <Logo type="default" />
+          <Item>
+            <Logo type="default" />
+          </Item>
         </Grid>
         <Grid item xs={4} md={4}>
-          <TextField
-            fullWidth
-            id="input-with-icon-textfield"
-            label="Text field"
-            InputProps={{
-              startAdornment: (
-                <InputAdornment position="start">
-                  <SearchIcon color="primary" />
-                </InputAdornment>
-              ),
-            }}
-            variant="standard"
-          />
+          <Item>
+            <TextField
+              fullWidth
+              id="input-with-icon-textfield"
+              label="Text field"
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position="start">
+                    <SearchIcon color="primary" />
+                  </InputAdornment>
+                ),
+              }}
+              variant="standard"
+            />
+          </Item>
         </Grid>
         <Grid item xs={4} md={4}>
-          <Link underline="none" color="primary">
-            Home
-          </Link>
-          <Link
-            underline="none"
-            className={styles.$text_primary}
-            sx={{ ml: 2, mr: 2 }}
-          >
-            Activity
-          </Link>
-          <Button variant="contained">Explore</Button>
+          <Item>
+            <Link underline="none" color="primary">
+              Home
+            </Link>
+            <Link
+              underline="none"
+              className={styles.$text_primary}
+              sx={{ ml: 2, mr: 2 }}
+            >
+              Activity
+            </Link>
+            <Button variant="contained">Explore</Button>
+          </Item>
         </Grid>
       </Grid>
     </Container>
