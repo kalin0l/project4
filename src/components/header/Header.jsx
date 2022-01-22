dd292c7bfc0ed2857ba03c99befb293f40477204
kalin0l <aleksiev2494@gmail.com>
2022-01-22 16:43:07 +0200

    changes in the imports

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,21 +1,20 @@
 import React from "react";
 import Grid from "@mui/material/Grid";
-import Container from "@mui/material/Container";
 import Logo from "../logo/Logo";
 import Button from "@mui/material/Button";
 import SearchIcon from "@mui/icons-material/Search";
 import TextField from "@mui/material/TextField";
 import InputAdornment from "@mui/material/InputAdornment";
-import styles from "./Header.module.css";
+import styles from "./Header.module.scss";
 
 export default function Header() {
   
   return (
       <Grid container maxWidth="xl">
-        <Grid item xs={4} md={4}>
+        <Grid item md={4}>
             <Logo type="default" />
         </Grid>
-        <Grid item xs={4} md={4}>
+        <Grid item md={4}>
             <TextField
               fullWidth
               id="input-with-icon-textfield"
@@ -30,12 +29,11 @@ export default function Header() {
               variant="standard"
             />
         </Grid>
-        <Grid item xs={4} md={4}>
+        <Grid item md={4}>
             <Button underline="none" color="primary">
               Home
             </Button>
             <Button
-              underline="none"
               className={styles.$text_primary}
               sx={{ ml: 2, mr: 2 }}
             >
