65ce15b080b99046e0c83cf1e52ae5b93d6b75e7
kalin0l <aleksiev2494@gmail.com>
2022-01-25 19:17:14 +0200

    likes change

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -4,21 +4,33 @@ import Avatar from "../avatar/Avatar";
 import avatarImg from "../../../public/images/avatar.png";
 import styles from "./Card.module.scss";
 import NFT from "../../../public/images/nft.jpg";
+import { Chip } from "@mui/material";
+import FavoriteIcon from '@mui/icons-material/Favorite';
 
 const { millify } = require("millify");
 
-export default function Card({ name='kalin', likes = 0, mediaUrl, price, currency='ETH' }) {
+export default function Card({
+  name = "kalin",
+  likes = 0,
+  mediaUrl,
+  price,
+  currency = "ETH",
+}) {
   return (
     <CardMui className={styles.card}>
       <Avatar url={avatarImg} verified={true} />
-      <img src={NFT.src} alt="nft" className={styles.media}/>
+      <img src={NFT.src} alt="nft" className={styles.media} />
       <div className={styles.details}>
         <div>
           <h5 className={styles.title}>{name}</h5>
-          <span className={styles.price}>~{millify(price)}{" "}{currency}</span>
+          <span className={styles.price}>
+            ~{millify(price)} {currency}
+          </span>
         </div>
         <div>
-          <span className={styles.likes}>{millify(likes)}</span>
+          <Chip icon={<FavoriteIcon/>} label={millify(likes)} color="success" variant="outlined">
+          
+          </Chip>
         </div>
       </div>
     </CardMui>
