126e77ffa89b852429c8c7d75e91009faf637a92
kalin0l <aleksiev2494@gmail.com>
2022-01-26 00:03:03 +0200

    adding countdown

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -5,35 +5,40 @@ import avatarImg from "../../../public/images/avatar.png";
 import styles from "./Card.module.scss";
 import NFT from "../../../public/images/nft.jpg";
 import { Chip } from "@mui/material";
-import FavoriteIcon from '@mui/icons-material/Favorite';
+import FavoriteIcon from "@mui/icons-material/Favorite";
 import millify from "millify";
-import Countdown from 'react-countdown';
-
-
+import Countdown from "react-countdown";
 
 export default function Card({
   name = "kalin",
   likes = 0,
-  mediaUrl='',
-  price=0,
+  mediaUrl = "",
+  price = 0,
   currency = "ETH",
-  timeLeft
+  timeLeft=5,
 }) {
   return (
-    <CardMui className={styles.card}>
-      <Avatar url={mediaUrl} verified={true} />
-      <img src={NFT.src} alt="nft" className={styles.media} />
-      <div className={styles.details}>
-        <div>
-          <h5 className={styles.title}>{name}</h5>
-          <span className={styles.price}>
-            ~{millify(price)} {currency}
-          </span>
+    <Countdown date={timeLeft}>
+      <CardMui className={styles.card}>
+        <Avatar url={mediaUrl} verified={true} />
+        <img src={NFT.src} alt="nft" className={styles.media} />
+        <div className={styles.details}>
+          <div>
+            <h5 className={styles.title}>{name}</h5>
+            <span className={styles.price}>
+              ~{millify(price)} {currency}
+            </span>
+          </div>
+          <div>
+            <Chip
+              label={millify(likes)}
+              className={styles.likes}
+              color="success"
+              variant="outlined"
+            />
+          </div>
         </div>
-        <div>
-          <Chip  label={millify(likes)} className={styles.likes} color="success" variant="outlined"/>
-        </div>
-      </div>
-    </CardMui>
+      </CardMui>
+    </Countdown>
   );
 }
