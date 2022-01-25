e8b4ceb184996c4cd5702628760825dda2109e70
kalin0l <aleksiev2494@gmail.com>
2022-01-25 23:33:04 +0200

    first check

---
+++ src/components/liveCard/LiveCard.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Card from "../card/Card";
+import styles from './LiveCard.module.scss'
+import LiveIcon from '@mui/icons-material/Live'
+
+export default function LiveCard(){
+    return (
+        <>
+        <Card title="" likes={} mediaUrl={} verified={true} price={} currency="" timeleft={5}/>
+        </>
+    )
+}
