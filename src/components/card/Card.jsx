0f217877ae49b38ef5da44071943441b46bd6bf9
kalin0l <aleksiev2494@gmail.com>
2022-01-25 18:19:17 +0200

    new changes

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,18 +1,25 @@
 import React from "react";
-import { Card as CardMui } from '@mui/material';
+import { Card as CardMui } from "@mui/material";
 import Avatar from "../avatar/Avatar";
 import avatarImg from "../../../public/images/avatar.png";
-import styles from './Card.module.scss'
+import styles from "./Card.module.scss";
+import NFT from "../../../public/images/nft.jpg";
+import millify from "millify";
 
-
-
-export default function Card({name,likes=0,mediaUrl,price,currency}) {
-    return (
-        <CardMui className={styles.card}>
-            <Avatar url={avatarImg}/>
-            <h5 className={styles.title}></h5>
-            <span className={styles.price}></span>
-            <span className={styles.likes}></span>
-        </CardMui>
-    )
+export default function Card({ name='kalin', likes = 1100, mediaUrl, price='12200', currency='ETH' }) {
+  return (
+    <CardMui className={styles.card}>
+      <Avatar url={avatarImg} verified={true} />
+      <img src={NFT.src} alt="nft" className={styles.media}/>
+      <div className={styles.details}>
+        <div>
+          <h5 className={styles.title}>{name}</h5>
+          <span className={styles.price}>{millify(price)}{" "}{currency}</span>
+        </div>
+        <div>
+          <span className={styles.likes}>{millify(likes)}</span>
+        </div>
+      </div>
+    </CardMui>
+  );
 }
