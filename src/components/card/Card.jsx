8f13dbd2c920e6b99062af730cda23081f55e69e
kalin0l <aleksiev2494@gmail.com>
2022-01-25 19:05:20 +0200

    price change

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -7,7 +7,7 @@ import NFT from "../../../public/images/nft.jpg";
 
 const { millify } = require("millify");
 
-export default function Card({ name='kalin', likes = 0, mediaUrl, price=12.2, currency='ETH' }) {
+export default function Card({ name='kalin', likes = 0, mediaUrl, price, currency='ETH' }) {
   return (
     <CardMui className={styles.card}>
       <Avatar url={avatarImg} verified={true} />
@@ -15,7 +15,7 @@ export default function Card({ name='kalin', likes = 0, mediaUrl, price=12.2, cu
       <div className={styles.details}>
         <div>
           <h5 className={styles.title}>{name}</h5>
-          <span className={styles.price}>~{millify(price)}{currency}</span>
+          <span className={styles.price}>~{millify(price)}{" "}{currency}</span>
         </div>
         <div>
           <span className={styles.likes}>{millify(likes)}</span>
