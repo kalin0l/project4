d7dcdd3f56904b80dfa5fa6900b9195c8fc7b798
kalin0l <aleksiev2494@gmail.com>
2022-01-26 18:47:38 +0200

    adding menuItem

--- src/components/step/Step.jsx
+++ src/components/step/Step.jsx
@@ -2,13 +2,14 @@ import React from "react";
 import styles from './Step.module.scss'
 import Grid from '@mui/material/Grid'
 import { Container } from "@mui/material";
+import { MenuItem } from "@mui/material";
 
 
 export default function Step({number=0,title='',description=''}){
     return (
         <Container sx={{ display: "flex" }}>
             <Grid item className={styles.bg}>
-                <span className={styles.number}>{number}</span>
+                <MenuItem className={styles.number}>{number}</MenuItem>
             </Grid>
             <Grid item>
                 <h1>{title}</h1>
