c562472601dd0f879a8667642269c4c4dbce8ea0
kalin0l <aleksiev2494@gmail.com>
2022-01-27 18:10:29 +0200

    done

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -3,6 +3,7 @@ import styles from "./Featured.module.scss";
 import { Container } from "@mui/material";
 import { ImageListItem } from "@mui/material";
 import { useRouter } from "next/router";
+import { ImageList } from "@mui/material";
 
 export default function Featured({ items = [] }) {
   const router = useRouter();
@@ -12,9 +13,9 @@ export default function Featured({ items = [] }) {
       <ImageListItem className={styles.wrapper}>
         {items.map((item, i) => {
           return (
-            <a key={i} id={i} href={item.href} onClick={() => router.push(item.href)} className={styles.img_container}>
+            <ImageList key={i} id={i} href={item.href} onClick={() => router.push(item.href)} className={styles.img_container}>
               <img src={item.image} alt={item.title} className={ i === 0 ? styles.img_active:styles.img}/>
-            </a>
+            </ImageList>
           );
         })}
       </ImageListItem>
