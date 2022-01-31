b0629ad6a2329a44189be75c5b9e3f90e57d41e3
kalin0l <aleksiev2494@gmail.com>
2022-01-31 18:10:04 +0200

    done

---
+++ src/components/product/ProductInfoPrice.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import classes from './ProductInfoPrice.module.scss'
+
+
+export default function ProductInfoPrice({amount=0,currency}){
+    return (
+        <div className={classes['product-info-price']}>
+        </div>
+    )
+}
