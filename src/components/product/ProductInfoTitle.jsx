deae03e582dc99ed6d35725db736f8408ad2648e
kalin0l <aleksiev2494@gmail.com>
2022-01-31 14:18:32 +0200

    adding classnames

--- src/components/product/ProductInfoTitle.jsx
+++ src/components/product/ProductInfoTitle.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import classes from "./ProductInfoTitle.module.scss";
 import { Typography } from "@mui/material";
+import classNames from 'classnames'
 
 export default function ProductInfoTitle({ text }) {
   return (
-    <div className={classes["product-info-title"]}>
+    <div className={classNames(classes["product-info-title"])}>
       <Typography className={classes.title}>
         <h1>{text}</h1>
       </Typography>
