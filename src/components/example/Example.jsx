49495b8f64d82903f74e7dd42a07a3f164a6f42f
kalin0l <aleksiev2494@gmail.com>
2022-02-03 08:25:06 +0200

    done

--- src/components/example/Example.jsx
+++ src/components/example/Example.jsx
@@ -22,6 +22,7 @@ import ProductInfoTitle from "../product/ProductInfoTitle";
 import ProductInfoPrice from "../product/ProductInfoPrice";
 import ProductInfoStatus from "../product/ProductInfoStatus";
 import ProductInfoCreator from "../product/ProductInfoCreator";
+import ProductInfoTimer from "../product/ProductInfoTimer";
 
 
 
@@ -67,7 +68,8 @@ export default function Copyright(props) {
         {/* <ProductInfoTitle text='text'/> */}
         {/* <ProductInfoPrice amount={3} currency={'ETH'}/> */}
         {/* <ProductInfoStatus/> */}
-        <ProductInfoCreator name="Kalin" avatar={avatarImg} verified={true}/>
+        {/* <ProductInfoCreator name="Kalin" avatar={avatarImg} verified={true}/> */}
+        <ProductInfoTimer timeEnd={10} onTimeEnd={10}/>
       </Container>
       <Footer/>
     </div>
