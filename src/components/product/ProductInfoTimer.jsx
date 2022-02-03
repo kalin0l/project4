c57f08046a47a6cb6ecc8b4b2c42ff8205a20e9c
kalin0l <aleksiev2494@gmail.com>
2022-02-03 07:38:03 +0200

    almost done

---
+++ src/components/product/ProductInfoTimer.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import classes from './ProductInfoTimer.module.scss'
+import Countdown from 'react-countdown'
+
+
+
+export default function ProductInfoTimer({timeEnd,onTimeEnd}){
+    return (
+        <div className={classes['product-info-timer class']}>
+            <div>
+
+            </div>
+
+            {timeEnd === null && <div className={classes.timer}>
+                <Countdown date={onTimeEnd}>
+                <h1 className={classes.title}>Ends in</h1>
+
+                </Countdown>
+            </div>}
+        </div>
+    )
+}
