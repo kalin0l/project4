fb3acda3e0bbf4ff262862590f1bd597133654a5
kalin0l <aleksiev2494@gmail.com>
2022-02-03 09:23:07 +0200

    d

--- src/components/product/ProductInfoTimer.jsx
+++ src/components/product/ProductInfoTimer.jsx
@@ -7,15 +7,17 @@ export default function ProductInfoTimer({ timeEnd, onTimeEnd }) {
   return (
     <div className={classes["product-info-timer"]}>
       <h1 className={classes.title}>Ends in</h1>
-      <div>
-        <Countdown date={timeEnd} onComplete={onTimeEnd} className={classes.active}>
-         
-        </Countdown>
-      </div>
-
-      {!timeEnd && <div className={classes.timer}>
-         timer-a
-          </div>}
+      {!timeEnd ? (
+        <div className={classes.timer}>timer-a</div>
+      ) : (
+        <div>
+          <Countdown
+            date={timeEnd}
+            onComplete={onTimeEnd}
+            className={classes.active}
+          ></Countdown>
+        </div>
+      )}
     </div>
   );
 }
