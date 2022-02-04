36f9921718c427945d68c3e1ec79d344351e0750
kalin0l <aleksiev2494@gmail.com>
2022-02-04 20:08:20 +0200

    done

---
+++ src/components/product/ProductInfo.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import classes from "./ProductInfo.module.scss";
+import Stack from "@mui/material/Stack";
+import ProductInfoPrice from "./ProductInfoPrice";
+import ProductInfoLikes from "./ProductInfoLikes";
+import ProductInfoCreator from "./ProductInfoCreator";
+import ProductInfoTimer from "./ProductInfoTimer";
+import ProductInfoStatus from "./ProductInfoStatus";
+import Grid from "@mui/material/Grid";
+
+export default function ProductInfo({
+  title,
+  creator,
+  price,
+  currency,
+  likes,
+  onTimeEnd,
+  timeEnd,
+  isLive,
+}) {
+  return (
+    <div className={classes["product-info"]}>
+      <Grid container>
+        <ProductInfoCreator />
+        <ProductInfoTimer />
+      </Grid>
+      <Stack spacing={2} className={classes.stats}>
+        <ProductInfoPrice />
+        <ProductInfoLikes />
+      </Stack>
+     {isLive && <ProductInfoStatus/>}
+    </div>
+  );
+}
